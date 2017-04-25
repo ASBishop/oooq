016903c40af3657036435464c990bfec0b8c3ad9
Alan Bishop <abishop@redhat.com>
2017-04-25 11:43:48 -0400

    Tweak support for deploying cinder NetApp back end
    Rename script to clearly indicate it's deploying a NetApp cinder back
    end, and use a local copy of the heat environment. Update the local
    environment file for new composable role.

--- deploy-netapp.sh
+++ deploy-netapp.sh
@@ -3,4 +3,4 @@ pushd ~ ; upload-puppet-modules -d puppet-modules ; popd
 time openstack overcloud deploy --templates ~/templates \
 --libvirt-type qemu \
 -e ~/tht/basic-layout.yaml \
--e ~/templates/environments/cinder-netapp-config.yaml
+-e ~/tht/cinder-nfs-config.yaml
